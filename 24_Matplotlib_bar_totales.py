9763259046528015ac809e1dbbeaca5f69dd13f5
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-09-13 11:25:25 -0300

    Add files via upload

---
+++ 24_Matplotlib_bar_totales.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Cargar los datos y preparar el DataFrame
+df = pd.read_csv(r'D:\Germán\Desktop\Python Files\migration.csv')
+df = df.dropna(axis=0)
+df['Value'] = df['Value'].astype(int)
+
+arg_esp = df[(df['Country of birth/nationality'] == 'Argentina') & (df['Country'] == 'Spain')]
+arg_esp = arg_esp.groupby('Year')['Value'].sum().reset_index()
+
+arg_ita = df[(df['Country of birth/nationality'] == 'Argentina') & (df['Country'] == 'Italy')]
+arg_ita = arg_ita.groupby('Year')['Value'].sum().reset_index()
+
+arg_usa = df[(df['Country of birth/nationality'] == 'Argentina') & (df['Country'] == 'United States')]
+arg_usa = arg_usa.groupby('Year')['Value'].sum().reset_index()
+
+# Preparar datos para el gráfico
+categorias = ['Hacia España', 'Hacia Italia', 'Hacia Estados Unidos']
+valores = [arg_esp['Value'].sum(), arg_ita['Value'].sum(), arg_usa['Value'].sum()]
+
+# Genera el gráfico de barras
+plt.bar(categorias, valores, color=['blue', 'green', 'red'], alpha=0.3)
+
+plt.ticklabel_format(style='plain', axis='y') # Configura el formato del eje y para evitar la notación científica
+plt.grid(True)  # Agregar una cuadrícula al gráfico
+plt.xlabel('Destino')
+plt.ylabel('Cantidad de Emigrantes')
+plt.title('Emigración Argentina a España, Italia y Estados Unidos')
+plt.tight_layout()  # Ajustar el diseño para evitar recortes en las etiquetas
+plt.show()
